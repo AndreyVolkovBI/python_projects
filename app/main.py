0cd92610e8d510ba83bf0fbd6449c56ee6e096e7
Andrey Volkov <AndreyVolkovHSEBI@yandex.ru>
2018-09-13 15:28:01 +0300

    'oneMore'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from flask import Flask, request, render_template
 from json import loads, dumps
+import os
 import Program as p
 import SearchLogic.Helper as h
 import DataBase.FireBase as fb
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
 
 @app.route("/")
 def index():
+    return 'Hello, world!'
+
     method = str(request.args.get('method'))  # post || simple || between || verified
     # user's params
     id = request.args.get('id')  # user's vk id
@@ -29,7 +33,7 @@ def index():
         output = p.getWay(int(fromId), h.getPureLink(toId), loads(friends))
     elif method == 'post':
         output = fb.postUserToDb(int(id), fullName, deviceModel, androidVersion)
-    elif 1 == 1:
+    else:
         return dumps(method) + " " + dumps(owner)
 
     if friends:
@@ -41,5 +45,5 @@ def index():
     return output
 
 if __name__ == '__main__':
-	app.run()
+	app.run(host='0.0.0.0')
 
