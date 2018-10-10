8bee077ae3387cc8471f42dd1f32cbd631d174a9
Andrey Volkov <AndreyVolkovHSEBI@yandex.ru>
2018-10-10 14:46:56 +0300

    'allsetisfied'

--- app/DataBase/FireBase.py
+++ app/DataBase/FireBase.py
@@ -28,7 +28,7 @@ def postUserToDb(id, fullName, deviceModel, androidVersion):
 
 # all requests post to global list of requests
 def postRequestToStorage(owner, method, fromId, toId, friends, content, time):
-    request = Request(owner, method, fromId, toId, friends, content, time)
+    request = Request(owner, method, fromId, toId, str(friends), content, time)
     request = request.__dict__
     requests.append(request)
     checkInterval()
